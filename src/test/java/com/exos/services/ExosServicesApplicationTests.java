c49efa27a011b070f68a901c0533c45e3e8be225
Rajupumpari2318 <rajupumpari99@gmail.com>
2025-06-19 17:38:34 +0100

    Update workflow and other changes

---
+++ src/test/java/com/exos/services/ExosServicesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.exos.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootTest
+class ExosServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
